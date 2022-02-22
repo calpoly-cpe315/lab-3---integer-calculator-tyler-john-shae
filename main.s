77f24fbf20551643d22af10f670aca8d71cde8b7
Shae Li <wo.1263202573@gmail.com>
2022-02-21 22:46:25 -0800

    Update main.s

--- main.s
+++ main.s
@@ -7,37 +7,61 @@
     .global main
 main:
     // driver function main lives here, modify this for your other functions
-	mov x0, #12
-	mov x1, #-4
+	//read the first number
+loop:
+	ldr	w0, =string0
+	bl	printf
+	ldr	w0, =formatInt
+	ldr	x1, =num1
+	bl	scanf
+	//read the second number
+	ldr	w0, =string1
+	bl	printf
+	ldr	w0, =formatInt
+	ldr	x1, =num2
+	bl	scanf
+	//read the operation
+	ldr	w0, =string2
+	bl	printf
+	ldr	w0, =formatStr
+	ldr	x1, =oper
+	bl	scanf
 
-	mov x20, #123
-	mov x21, #456
-	bl intmul
-	mov x1, x0
-	mov x2, x20
-	mov x3, x21
-	ldr x0, =printChar 
-	bl printf
-	mov x0, #0
+	//calculate
+
+
+
+	//print result
+	ldr	w0, =string3
+	ldr	x1, =num2
+	ldr	x1, [x1, #0]
+	bl	printf
+
+	//print agian?
+	ldr	w0, =string4
+	bl	printf
+	
+	ldr	w0, =formatStr
+	mov	x1, sp
+	bl	scanf
+	ldr	x1, =yes
+	ldrb	w1, [x1]
+	ldrb	w0, [sp]
+	cmp	w0, w1
+	b	loop
 	ret
 
+.data
+yes: .byte 'y'
+formatInt: .asciz "%d"
+formatStr: .asciz " %c"
+num1: .word 0
+num2: .word 0
+oper: .word 0
+string0: .asciz "Enter Number 1: "
+string1: .asciz "Enter Number 2: "
+string2: .asciz "Enter Operation: "
+string3: .asciz "Result is: %d\n"
+string4: .asciz "Again? "
+string5: .asciz "Invalid Operation Entered.\n"
 
-    // You'll need to scan characters for the operation and to determine
-    // if the program should repeat.
-    // To scan a character, and compare it to another, do the following
-loop: ldr     w0, =scanchar
-      mov     x1, sp          // Save stack pointer to x1, you must create space
-      bl      scanf           // Scan user's answer
-      ldr     x1, =yes        // Put address of 'y' in x1
-      ldrb    w1, [x1]        // Load the actual character 'y' into x1
-      ldrb    w0, [sp]        // Put the user's value in r0
-      cmp     w0, w1          // Compare user's answer to char 'y'
-      b       loop            // branch to appropriate location
-
-yes:
-    .byte   'y'
-scanchar:
-    .asciz  " %c"
-
-printChar:
-.asciz "Num: %d, %d, %d\n"
